0b59cb04c9411e041f7c9b360eb8036834ca8d60
jeleencubillas <jpclavecilla@gmail.com>
2021-03-02 21:42:06 +0800

    Created User and Admin Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  get "/pages/:page" => "pages#show"
+  root "pages#show", page: "home"
+
+  devise_for :admins
+  devise_for :users
 end
