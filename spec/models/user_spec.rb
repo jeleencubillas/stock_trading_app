23efae2d13d809459f4b18456be2c5e255669c9e
Manuel <mrctinio@gmail.com>
2021-03-09 19:52:57 +0800

    add confirmable column to user and generate views for buyer and broker

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,13 +22,14 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
     end
 
-    it "Shoud not save without age" do
+    it "Shoud not save if age < 18" do
       @user = User.new()
       @user.email = 'testing@email.com'
       @user.password = '123456'
       @user.last_name = 'Tester'
       @user.first_name = 'Test'
-
+      @user.date_of_birth = '03/12/2015'
+      
       expect(@user).to_not be_valid
     end
   end
