cf470ad59ada70b54147514faf19de8361fd9391
Manuel <mrctinio@gmail.com>
2021-03-09 20:13:20 +0800

    update user.rb to include age validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,29 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
          :confirmable
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :date_of_birth, presence: true
+         
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :date_of_birth, presence: true
+
+  before_validation :validate_age
 
   enum role: [:buyer, :broker]
 
   def set_default_role
     self.role ||= :buyer
   end
+
+  def validate_age
+    now = Date.today
+    before = date_of_birth
+    difference_in_days = (now - before).to_i
+  
+    if date_of_birth.present? && (difference_in_days/365.25).to_i <= 18
+      errors.add('You should be over 18 years old to register')
+    end
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_150819) do
+ActiveRecord::Schema.define(version: 2021_03_09_113717) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 2021_03_07_150819) do
     t.string "first_name"
     t.string "last_name"
     t.date "date_of_birth"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
