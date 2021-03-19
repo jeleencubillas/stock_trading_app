32e3bed002826da3fde3da5b82803d9180233195
Manuel <mrctinio@gmail.com>
2021-03-19 21:22:39 +0800

    add search function and add function for stocks to user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'stocks/index'
   get 'stocks/show'
   get 'stocks/search'
+  post 'stocks/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/pages/:page" => "pages#show"
   root "pages#show", page: "home"
